aa52add1ce332fd6ddf7189e7aa850c710a6a557
Yaowen Mei <ywmei@google.com>
2024-02-08 21:41:52 +0000

    Support Javac argument file
    Previously, if one use javac @filename, our flag parser expect all the
    flags in @filename are input files.
    
    This CL ported from this clanglink CL(http://tg/1719633), will extended
    the support of javac @filename, so that you can put other command flags
    , such as -classpath a:b:c into the @filename
    
    Test: Unit test and CI Android build: http://shortn/_BbwwM5awnH
    Bug: b/323030375
    Change-Id: Ie4b5c8ea364b76db6b0a203ca76a05fea8a0f2b4
    GitOrigin-RevId: d57eaa9c925836e9be1db65c855faf061cc6ad7a

--- internal/pkg/inputprocessor/action/javac/flagsparser.go
+++ internal/pkg/inputprocessor/action/javac/flagsparser.go
@@ -52,7 +52,27 @@ func parseFlags(ctx context.Context, command []string, workingDir, execRoot stri
 		},
 	}
 	for s.HasNext() {
-		flag, args, values, _ := s.Next()
+		err := handleJavacFlags(s.NextResult(), res, &s)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return res, nil
+}
+
+func handleJavacFlags(nextRes *args.NextResult, flgs *flags.CommandFlags, scanner *args.Scanner) error {
+	// A temporary CommandFlags structure is used to collect the various dependencies, flags and outputs in the handleArgFunc,
+	// and then the results of that are copied into the CommandFlags structure passed in (flgs).
+	// This is done because if a flag is being processed that is in a rsp file, we don't want to add that flag to
+	// the Flags list; otherwise, the flags will show up on the remote bot twice, one time in the cmd itself,
+	// the second time inside the rsp file. Perhaps a cleaner option is to add a
+	// flag to the handleArgFunc that indicates if the argument is from the command line or from inside a rsp file.
+	f := &flags.CommandFlags{
+		ExecRoot:         flgs.ExecRoot,
+		WorkingDirectory: flgs.WorkingDirectory,
+	}
+	handleArgFunc := func(arg *args.NextResult) error {
+		flag, args, values := arg.NormalizedKey, arg.Args, arg.Values
 		switch flag {
 		case "-bootclasspath", "-classpath", "-processorpath":
 			deps := strings.Split(values[0], ":")
@@ -61,28 +81,45 @@ func parseFlags(ctx context.Context, command []string, workingDir, execRoot stri
 				if d == "" || d == "." {
 					continue
 				}
-				res.Dependencies = append(res.Dependencies, d)
+				f.Dependencies = append(f.Dependencies, d)
 			}
-			continue
 		case "--system=", "-Aroom.schemaLocation=":
-			res.Dependencies = append(res.Dependencies, values[0])
-			continue
+			f.Dependencies = append(f.Dependencies, values[0])
 		case "-d", "-s":
-			res.OutputDirPaths = append(res.OutputDirPaths, values[0])
-			continue
+			f.OutputDirPaths = append(f.OutputDirPaths, values[0])
 		case "":
-			if strings.HasPrefix(args[0], "@") {
-				rspFile := args[0][1:]
-				res.TargetFilePaths = append(res.TargetFilePaths, rspFile)
-				rspDeps, err := rsp.Parse(filepath.Join(execRoot, workingDir, rspFile))
-				if err != nil {
-					return nil, err
-				}
-				res.Dependencies = append(res.Dependencies, rspDeps...)
-				continue
-			}
+			f.Dependencies = append(f.Dependencies, args[0])
 		}
-		res.Flags = append(res.Flags, &flags.Flag{Value: args[0]})
+
+		for _, arg := range args {
+			f.Flags = append(f.Flags, &flags.Flag{Value: arg})
+		}
+		return nil
 	}
-	return res, nil
+	// Check if this is a rsp file that needs processing or just a normal flag.
+	if strings.HasPrefix(nextRes.Args[0], "@") {
+		rspFile := nextRes.Args[0][1:]
+		flgs.TargetFilePaths = append(flgs.TargetFilePaths, rspFile)
+		if !filepath.IsAbs(rspFile) {
+			rspFile = filepath.Join(flgs.ExecRoot, flgs.WorkingDirectory, rspFile)
+		}
+		flgs.Flags = append(flgs.Flags, &flags.Flag{Value: nextRes.Args[0]})
+		err := rsp.ParseWithFunc(rspFile, *scanner, handleArgFunc)
+		if err != nil {
+			return err
+		}
+		// We don't want to pass along the flags that were in the rsp file, just the
+		// rsp file itself as a flag.
+	} else {
+		err := handleArgFunc(nextRes)
+		if err != nil {
+			return err
+		}
+		// We want to pass along flags that were on the command line.
+		flgs.Flags = append(flgs.Flags, f.Flags...)
+	}
+	flgs.Dependencies = append(flgs.Dependencies, f.Dependencies...)
+	flgs.OutputDirPaths = append(flgs.OutputDirPaths, f.OutputDirPaths...)
+	flgs.OutputFilePaths = append(flgs.OutputFilePaths, f.OutputFilePaths...)
+	return nil
 }
--- internal/pkg/inputprocessor/action/javac/preprocessor_test.go
+++ internal/pkg/inputprocessor/action/javac/preprocessor_test.go
@@ -64,3 +64,56 @@ func TestJavacPreprocessor(t *testing.T) {
 		t.Errorf("GetActionSpec() returned diff in ActionSpec, (-want +got): %s", diff)
 	}
 }
+
+func TestJavacArgumentFilesPreprocessor(t *testing.T) {
+	er, cleanup := execroot.Setup(t, []string{"MyClass1.java", "MyClass2.java", "MyClass3.java", "MyClass4.java", "MyClass5.java", "MyClass6.java", "a", "b", "c", "d", "e", "f", "g", "j", "k", "lib", "javac"})
+	defer cleanup()
+	// See Example here: https://docs.oracle.com/en/java/javase/17/docs/specs/man/javac.html#examples-of-using-javac-filename
+	// 1) Use of the @<filename> to recursively interpret files is not supported.
+	// 2) The arguments within a file can be separated by spaces or new line characters.
+	// TODO(b/324585908): Support flag value with embedded spaces enclosed by double quotation marks.
+	// 3) If a file name contains embedded spaces, then put the whole file name in double quotation marks.
+	execroot.AddFileWithContent(t, filepath.Join(er, "options"), []byte("-bootclasspath a:b:c\n-classpath d:e:f\n-processorpath g\n--system=j\n-Aroom.schemaLocation=k\n-d out1/\n-s out2/"))
+	execroot.AddFileWithContent(t, filepath.Join(er, "foo/bar/baz/classes"), []byte("MyClass1.java\nMyClass2.java\nMyClass3.java"))
+	execroot.AddFileWithContent(t, filepath.Join(er, "classes_seperated_by_space"), []byte("MyClass4.java MyClass5.java\n\"MyClass6.java\""))
+	execroot.AddFileWithContent(t, filepath.Join(er, "javac_remote_toolchain_inputs"), []byte("lib"))
+	cmd := []string{"javac", "@options", "@foo/bar/baz/classes", "@classes_seperated_by_space"}
+	ctx := context.Background()
+	pp := &Preprocessor{
+		&inputprocessor.BasePreprocessor{
+			Ctx: ctx,
+		},
+	}
+	gotSpec, err := inputprocessor.Compute(pp, inputprocessor.Options{
+		ExecRoot:        er,
+		ToolchainInputs: []string{"javac"},
+		Cmd:             cmd,
+	})
+	if err != nil {
+		t.Errorf("Compute() returned error: %v", err)
+	}
+	wantSpec := &inputprocessor.ActionSpec{
+		InputSpec: &command.InputSpec{
+			Inputs: []string{
+				"options",
+				filepath.Join("foo", "bar", "baz", "classes"),
+				"MyClass1.java",
+				"MyClass2.java",
+				"MyClass3.java",
+				"classes_seperated_by_space",
+				"MyClass4.java",
+				"MyClass5.java",
+				"MyClass6.java",
+				"a", "b", "c", "d", "e", "f", "g", "j", "k",
+				"lib",
+				"javac"},
+			EnvironmentVariables: map[string]string{
+				"PATH": ".:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+			},
+		},
+		OutputDirectories: []string{"out1", "out2"},
+	}
+	if diff := cmp.Diff(wantSpec, gotSpec, strSliceCmp); diff != "" {
+		t.Errorf("GetActionSpec() returned diff in ActionSpec, (-want +got): %s", diff)
+	}
+}
